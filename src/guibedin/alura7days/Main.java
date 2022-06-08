188e1e544ba9a91a49ca8f39325acdc325de5d73
Guilherme Bedin <guilherme0bedin@gmail.com>
2022-06-08 16:50:39 -0300

    day2 - manual parse of movies (#2)

--- src/guibedin/alura7days/Main.java
+++ src/guibedin/alura7days/Main.java
@@ -4,10 +4,34 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.rmi.server.ExportException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
+    // Split attributes from a single movie string
+    public static String[] splitAttributes(String movies) {
+        movies = movies.replace("{", "");
+        movies = movies.replace("}", "");
+
+        return movies.split("\",\"");
+    }
+
+    // Parse the specified attribute, creating a list of the same attribute from all movies
+    public static List<String> parseAttribute(String[] movies, String attribute) {
+        List<String> attributes = new ArrayList<>();
+
+        for(String movie : movies) {
+            String[] attributesArray = splitAttributes(movie);
+            for(String atr : attributesArray) {
+                if(atr.contains(attribute)) {
+                    attributes.add(atr.split(":\"")[1].replace("\"",""));
+                }
+            }
+        }
+
+        return attributes;
+    }
     public static void main(String[] args) {
 
         // Make a GET HTTP Request to IMDB API and print the response in stdout
@@ -21,9 +45,53 @@ public class Main {
                 .build();
         HttpResponse<String> response;
 
+        // Parse JSON response into multiple lists
+        List<String> ids;
+        List<String> ranks;
+        List<String> titles;
+        List<String> fullTitles;
+        List<String> years;
+        List<String> urlImages;
+        List<String> crews;
+        List<String> imDbRatings;
+        List<String> imDbRatingCounts;
+
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
+            String body = response.body();
+            int start = body.indexOf("[") + 1;
+            int end = body.indexOf("]");
+
+            String moviesJson = body.substring(start, end);
+            String[] moviesArray = moviesJson.split("},");
+
+            ids = parseAttribute(moviesArray, "id");
+            ids.forEach(System.out::println);
+
+            ranks = parseAttribute(moviesArray, "rank");
+            ranks.forEach(System.out::println);
+
+            titles = parseAttribute(moviesArray, "title");
+            titles.forEach(System.out::println);
+
+            fullTitles = parseAttribute(moviesArray, "fullTitle");
+            fullTitles.forEach(System.out::println);
+
+            years = parseAttribute(moviesArray, "year");
+            years.forEach(System.out::println);
+
+            urlImages = parseAttribute(moviesArray, "urlImage");
+            urlImages.forEach(System.out::println);
+
+            crews = parseAttribute(moviesArray, "crew");
+            crews.forEach(System.out::println);
+
+            imDbRatings = parseAttribute(moviesArray, "imDbRating");
+            imDbRatings.forEach(System.out::println);
+
+            imDbRatingCounts = parseAttribute(moviesArray, "imDbRatingCount");
+            imDbRatingCounts.forEach(System.out::println);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
