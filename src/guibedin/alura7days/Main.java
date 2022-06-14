fcdf0d9e428d51118f4a5dbe8fe81ec041eb8897
Guilherme Bedin <guilherme0bedin@gmail.com>
2022-06-14 17:54:24 -0300

    feat(day5): creating classes for API Client and Parser (#5)

--- src/guibedin/alura7days/Main.java
+++ src/guibedin/alura7days/Main.java
@@ -4,6 +4,7 @@ import guibedin.alura7days.day3.jackson.Day3Jackson;
 import guibedin.alura7days.day3.parse.Day3;
 import guibedin.alura7days.day3.record.Day3Record;
 import guibedin.alura7days.day4.Day4;
+import guibedin.alura7days.day5.Day5;
 
 public class Main {
 
@@ -16,7 +17,8 @@ public class Main {
 //            Day3Jackson.execute();
 //            System.out.println("==============================");
 //            Day3Record.execute();
-            Day4.execute();
+//            Day4.execute();
+            Day5.execute();
         } catch(Exception e) {
             e.printStackTrace();
         }
